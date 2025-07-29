257f5715b9a92b0a5ff6870ae480b0cbb592763e
Manuel Negrin <manu.negrin17@gmail.com>
2025-07-29 16:44:35 -0300

    Cart total and scroll

--- src/components/CartDrawer.jsx
+++ src/components/CartDrawer.jsx
@@ -3,30 +3,42 @@ import "../AboutUs.css";
 export default function CartDrawer({ isOpen, cartItems, onQuantityChange }) {
   return (
     <div className={`cart-drawer ${isOpen ? "open" : ""}`}>
-      <div className="cart-header">
-        <h2>Tu carrito</h2>
+      <div className="border-bottom pb-2 mb-3">
+        <h2 className="h5 m-0">Your cart</h2>
       </div>
 
-      <div className="text-content">
+      <div className="flex-grow-1 overflow-auto">
         {cartItems.length === 0 ? (
           <p>Tu carrito está vacío.</p>
         ) : (
-          cartItems.map((item, index) => (
-            <div key={index} className="cart-item d-flex">
-              <span>{item.name}</span>
-              <input
-                className="col-2"
-                type="number"
-                value={item.quantity}
-                min={1}
-                onChange={(num) =>
-                  onQuantityChange(item.id, parseInt(num.target.value))
-                }
-              />
-              <span>{item.price * item.quantity} UYU</span>
-            </div>
-          ))
+          <div className="d-flex flex-column gap-3">
+            {cartItems.map((item, index) => (
+              <div key={index} className="cart-item d-flex">
+                <span>{item.name}</span>
+                <input
+                  className="form-control form-control-sm w-25 mx-2"
+                  type="number"
+                  value={item.quantity}
+                  min={1}
+                  onChange={(num) =>
+                    onQuantityChange(item.id, parseInt(num.target.value))
+                  }
+                />
+                <span>${item.price * item.quantity}</span>
+              </div>
+            ))}
+          </div>
         )}
+      </div>
+      <div className="border-top pt-3 mt-3 d-flex justify-content-between align-items-center">
+        <strong>Total</strong>
+        <strong>
+          $
+          {cartItems.reduce(
+            (total, item) => total + item.price * item.quantity,
+            0
+          )}{" "}
+        </strong>
       </div>
     </div>
   );
