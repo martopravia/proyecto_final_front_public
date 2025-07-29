a1cf4174093bb00ac7a6e0f5b0a48ebed7c3c74e
Martín Pravia <marto.pravia@gmail.com>
2025-07-29 14:06:54 -0300

    cambio de ruta Footer a componente, rapeo en div y main las routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <Routes>
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/register" element={<Register />} /> */}
-        {/* <Route path="/" element={<Home />} /> */}
-        {/* <Route path="/cart" element={<Checkout />} /> */}
-        {/* <Route path="/checkout" element={<Checkout />} /> */}
-        {/* <Route path="/products" element={<Products />} /> */}
-        {/* <Route path="/products/:productId" element={<Product />} /> */}
-        {/* <Route path="/:username" element={<Profile />} /> */}
-        <Route path="*" element={<p>There's nothing here: 404!</p>} />
-      </Routes>
-      <Route path="/footer" element={<Footer />} />
+      <div className="d-flex flex-column min-vh-100">
+        <main className="flex-grow-1">
+          <Routes>
+            {/* <Route path="/login" element={<Login />} /> */}
+            {/* <Route path="/register" element={<Register />} /> */}
+            {/* <Route path="/" element={<Home />} /> */}
+            {/* <Route path="/cart" element={<Checkout />} /> */}
+            {/* <Route path="/checkout" element={<Checkout />} /> */}
+            {/* <Route path="/products" element={<Products />} /> */}
+            {/* <Route path="/products/:productId" element={<Product />} /> */}
+            {/* <Route path="/:username" element={<Profile />} /> */}
+            <Route path="*" element={<p>There's nothing here: 404!</p>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
