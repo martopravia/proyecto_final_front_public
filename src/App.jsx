61756ea83ded8b99a0a9de5f427276b90a2240b4
Juan Pedro Diaz Lumaca <juanpediaz99@gmail.com>
2025-07-29 12:22:14 -0300

    navbar responsive con hamburguesa

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import {Routes , Route} from "react-router-dom";
+import Navbar from "./components/AppNavbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         {/* <Route path="/login" element={<Login />} /> */}
         {/* <Route path="/register" element={<Register />} /> */}
