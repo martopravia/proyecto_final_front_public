f4a379dbab2f297c1516395e430b34cea6bc72bb
Martín Pravia <marto.pravia@gmail.com>
2025-07-29 14:58:51 -0300

    se traen componentes

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 export default function AppNavbar() {
