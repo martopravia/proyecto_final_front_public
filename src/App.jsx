585fcd3a9907246c74bd601d47c6c1067ad1e509
Martín Pravia <marto.pravia@gmail.com>
2025-07-31 12:08:11 -0300

    re arregla app

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,14 @@ import CategoryPage from "./components/CategoryPage";
 import Home from "./components/Home";
 import ProductList from "./components/ProductList";
 
-
 function App() {
   return (
     <>
-
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} 
+          <Route path="register" element={<Register />} />
           {/* <Route path="/cart" element={<Checkout />} /> */}
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/products" element={<ProductList />} />
@@ -47,7 +45,6 @@ function App() {
         theme="light"
         transition={Bounce}
       />
-
     </>
   );
 }
